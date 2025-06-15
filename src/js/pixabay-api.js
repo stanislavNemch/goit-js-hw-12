0e5bab66d4e6ffcc7da7a410d4e545c5bd188b5a
Stanislav Nemchenko <stas.buka@gmail.com>
2025-06-15 13:43:37 +0300

    add filter by war tags

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const forbiddenTags = ['tank', 'war', 'danger', 'military', 'army', 'battle'];
+
 export async function getImagesByQuery(query, page = 1) {
   // Додано параметр page
   const API_KEY = '50678696-ed6f097088bf5690dd98584b9';
@@ -18,7 +20,14 @@ export async function getImagesByQuery(query, page = 1) {
 
   try {
     const response = await axios.get(BASE_URL, { params });
-    return response.data;
+    const filteredHits = response.data.hits.filter(hit => {
+      const tagsArray = hit.tags
+        .toLowerCase()
+        .split(',')
+        .map(tag => tag.trim());
+      return !forbiddenTags.some(forbidden => tagsArray.includes(forbidden));
+    });
+    return { ...response.data, hits: filteredHits };
   } catch (error) {
     console.error('Error fetching images:', error);
     throw new Error('Failed to fetch images from Pixabay.');
